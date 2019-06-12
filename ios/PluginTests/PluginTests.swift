02d4d5d47438521ad2c364751a3c209664f70bde
Jeremy Li <mail.jeremy.li@gmail.com>
2019-06-12 16:29:34 +1000

    unit tests using promises

--- ios/PluginTests/PluginTests.swift
+++ ios/PluginTests/PluginTests.swift
@@ -1,5 +1,7 @@
 import XCTest
 import Capacitor
+import Promises
+
 @testable import Plugin
 
 class PluginTests: XCTestCase {
@@ -17,113 +19,253 @@ class PluginTests: XCTestCase {
   func testSetUp() {
     
     let plugin = SegmentPlugin()
+    let expectation = XCTestExpectation(description: "[SET_UP]");
     
+    Promise(()).then({
 
-    ({
-      let expectation = XCTestExpectation(description: "[SET_UP] NO_KEY");
-      let call = CAPPluginCall(callbackId: "test", options: [:], success: { (result, call) in
-        XCTFail("🤦‍♂️ Success callback should not have been called with empty key")
-        expectation.fulfill()
-      }, error: { (err: CAPPluginCallError?) in
-        XCTAssertEqual(err!.message, "segment key not specified");
-        XCTAssertEqual(err!.data!["code"]! as? String, "[SET_UP] NO_KEY");
-        //      print("👎 err message: " + err!.message + ". data: " + err!.data!.description);
-        expectation.fulfill()
-      });
-      plugin.setUp(call!)
-      wait(for: [expectation], timeout: 1);
-    })();
+      // "[SET_UP] NO_KEY"
 
+      return Promise<Void>(on: .main) { (fulfill, reject) in
+        let call = CAPPluginCall(callbackId: "test", options: [:], success: { (result, call) in
+          XCTFail("🤦‍♂️ Success callback should not have been called with empty key")
+          fulfill(());
+        }, error: { (err: CAPPluginCallError?) in
+          XCTAssertEqual(err!.data!["code"]! as? String, "[SET_UP] NO_KEY");
+          fulfill(());
+        });
+        plugin.setUp(call!);
+      };
 
-    ({
-      let expectation = XCTestExpectation(description: "[SET_UP] SUCCESS");
-      let call = CAPPluginCall(callbackId: "test", options: [
-        "key": "test_segment_key"
-      ], success: { (result, call) in
-        XCTAssertEqual(result!.data.count, 0);
-        expectation.fulfill()
-      }, error: { (err: CAPPluginCallError?) in
-        XCTFail("🤦‍♂️ Error callback should not have been called")
-        expectation.fulfill()
-      });
-      plugin.setUp(call!)
-      wait(for: [expectation], timeout: 1);
-    })();
+    }).then({
 
-    
-    ({
-      let expectation = XCTestExpectation(description: "[SET_UP] DUPE_CALL");
-      let call = CAPPluginCall(callbackId: "test", options: [
-        "key": "test_segment_key"
+      // "[SET_UP] SUCCESS"
+
+      return Promise<Void>(on: .main) { (fulfill, reject) in
+        let call = CAPPluginCall(callbackId: "test", options: [
+          "key": "test_segment_key"
+        ], success: { (result, call) in
+          XCTAssertEqual(result!.data.count, 0);
+          fulfill(());
+        }, error: { (err: CAPPluginCallError?) in
+          XCTFail("🤦‍♂️ Error callback should not have been called")
+          fulfill(());
+        });
+        plugin.setUp(call!);
+      };
+
+    }).then({
+
+      // "[SET_UP] DUPE_CALL"
+
+      return Promise<Void>(on: .main) { (fulfill, reject) in
+        let call = CAPPluginCall(callbackId: "test", options: [
+          "key": "test_segment_key"
         ], success: { (result, call) in
           XCTFail("🤦‍♂️ Success callback should not have been called")
-          expectation.fulfill()
-      }, error: { (err: CAPPluginCallError?) in
-        XCTAssertEqual(err!.message, "segment already set up");
-        XCTAssertEqual(err!.data!["code"]! as? String, "[SET_UP] DUPE_CALL");
-        expectation.fulfill()
-      });
-      plugin.setUp(call!)
-      wait(for: [expectation], timeout: 1);
-    })();
+          fulfill(());
+        }, error: { (err: CAPPluginCallError?) in
+          XCTAssertEqual(err!.data!["code"]! as? String, "[SET_UP] DUPE_CALL");
+          fulfill(());
+        });
+        plugin.setUp(call!);
+      };
+
+    }).then({
+      expectation.fulfill();
+    });
+
+    self.wait(for: [expectation], timeout: 1);
     
     // TODO: test location services and lifecycle
 
   }
   
+  
   func testIdentify() {
+
+    let plugin = SegmentPlugin();
+    let expectation = XCTestExpectation(description: "[IDENTIFY]");
+
+    Promise(()).then({
+
+      // [IDENTIFY] NOT_SET_UP
+
+      return Promise<Void>(on: .main) { (fulfill, reject) in
+        let call = CAPPluginCall(callbackId: "test", options: [:], success: { (result, call) in
+          XCTFail("🤦‍♂️ Success callback should not have been called")
+          fulfill(());
+        }, error: { (err: CAPPluginCallError?) in
+          XCTAssertEqual(err!.data!["code"]! as? String, "[IDENTIFY] NOT_SET_UP");
+          fulfill(());
+        });
+        plugin.identify(call!);
+      };
+
+    }).then({
+
+      return self._setUpForTest(plugin: plugin);
+
+    }).then({ () -> Promise<Void> in
+
+      // [IDENTIFY] NO_USER_ID
+      
+      return Promise<Void>(on: .main) { (fulfill, reject) in
+        let call = CAPPluginCall(callbackId: "test", options: [:], success: { (result, call) in
+          XCTFail("🤦‍♂️ Success callback should not have been called")
+          fulfill(());
+        }, error: { (err: CAPPluginCallError?) in
+          XCTAssertEqual(err!.data!["code"]! as? String, "[IDENTIFY] NO_USER_ID");
+          fulfill(());
+        });
+        plugin.identify(call!);
+      };
+
+    }).then({ () -> Promise<Void> in
+
+      // [IDENTIFY] SUCCESS
+      
+      return Promise<Void>(on: .main) { (fulfill, reject) in
+        let call = CAPPluginCall(callbackId: "test", options: [
+          "userID": "jeremy_test_id",
+        ], success: { (result, call) in
+          fulfill(());
+        }, error: { (err: CAPPluginCallError?) in
+          XCTFail("🤦‍♂️ Failure callback should not have been called");
+          fulfill(());
+        });
+        plugin.identify(call!);
+      };
+
+    }).then({
+      expectation.fulfill();
+    });
+    
+    // TODO: TEST TRAITS
+    
+    self.wait(for: [expectation], timeout: 1);
+
+  }
+  
+  func testTrack() {
     
     let plugin = SegmentPlugin();
-
+    let expectation = XCTestExpectation(description: "[TRACK]");
     
-    ({
-      let expectation = XCTestExpectation(description: "[IDENTIFY] NOT_SET_UP");
-      let call = CAPPluginCall(callbackId: "test", options: [:], success: { (result, call) in
-        XCTFail("🤦‍♂️ Success callback should not have been called")
-        expectation.fulfill()
+    Promise(()).then({
+      
+      // [TRACK] NOT_SET_UP
+      
+      return Promise<Void>(on: .main) { (fulfill, reject) in
+        let call = CAPPluginCall(callbackId: "test", options: [:], success: { (result, call) in
+          XCTFail("🤦‍♂️ Success callback should not have been called");
+          fulfill(());
+        }, error: { (err: CAPPluginCallError?) in
+          XCTAssertEqual(err!.data!["code"]! as? String, "[TRACK] NOT_SET_UP");
+          fulfill(());
+        });
+        plugin.track(call!);
+      };
+      
+    }).then({
+      
+      return self._setUpForTest(plugin: plugin);
+      
+    }).then({ () -> Promise<Void> in
+      
+      // [TRACK] NO_USER_ID
+      
+      return Promise<Void>(on: .main) { (fulfill, reject) in
+        let call = CAPPluginCall(callbackId: "test", options: [:], success: { (result, call) in
+          XCTFail("🤦‍♂️ Success callback should not have been called");
+          fulfill(());
+        }, error: { (err: CAPPluginCallError?) in
+          XCTAssertEqual(err!.data!["code"]! as? String, "[TRACK] NO_EVENT_NAME");
+          fulfill(());
+        });
+        plugin.track(call!);
+      };
+      
+    }).then({ () -> Promise<Void> in
+      
+      // [TRACK] SUCCESS
+      
+      return Promise<Void>(on: .main) { (fulfill, reject) in
+        let call = CAPPluginCall(callbackId: "test", options: [
+          "eventName": "test_event_name",
+        ], success: { (result, call) in
+          fulfill(());
+        }, error: { (err: CAPPluginCallError?) in
+          XCTFail("🤦‍♂️ Failure callback should not have been called");
+          fulfill(());
+        });
+        plugin.track(call!);
+      };
+      
+    }).then({
+      expectation.fulfill();
+    });
+    
+    // TODO: TEST PROPERTIES, OPTIONS
+    
+    self.wait(for: [expectation], timeout: 1);
+    
+  }
+  
+  func testReset() {
+    let plugin = SegmentPlugin();
+    let expectation = XCTestExpectation(description: "[TRACK]");
+    
+    Promise(()).then({
+      
+      // [RESET] NOT_SET_UP
+      
+      return Promise<Void>(on: .main) { (fulfill, reject) in
+        let call = CAPPluginCall(callbackId: "test", options: [:], success: { (result, call) in
+          XCTFail("🤦‍♂️ Success callback should not have been called");
+          fulfill(());
+        }, error: { (err: CAPPluginCallError?) in
+          XCTAssertEqual(err!.data!["code"]! as? String, "[RESET] NOT_SET_UP");
+          fulfill(());
+        });
+        plugin.reset(call!);
+      };
+      
+    }).then({
+      
+      return self._setUpForTest(plugin: plugin);
+      
+    }).then({ () -> Promise<Void> in
+      
+      // [RESET] SUCCESS
+      
+      return Promise<Void>(on: .main) { (fulfill, reject) in
+        let call = CAPPluginCall(callbackId: "test", options: [:], success: { (result, call) in
+          fulfill(());
+        }, error: { (err: CAPPluginCallError?) in
+          XCTFail("🤦‍♂️ Failure callback should not have been called");
+          fulfill(());
+        });
+        plugin.reset(call!);
+      };
+      
+    }).then({
+      expectation.fulfill();
+    });
+    
+    self.wait(for: [expectation], timeout: 1);
+  }
+  
+  private func _setUpForTest(plugin: SegmentPlugin) -> Promise<Void> {
+    return Promise<Void>(on: .main) { (fulfill, reject) in
+      let call = CAPPluginCall(callbackId: "test", options: [
+        "key": "test_segment_key"
+      ], success: { (result, call) in
+        fulfill(());
       }, error: { (err: CAPPluginCallError?) in
-        XCTAssertEqual(err!.message, "segment not set up");
-        XCTAssertEqual(err!.data!["code"]! as? String, "[IDENTIFY] NOT_SET_UP");
-        expectation.fulfill()
+        fulfill(());
       });
-      plugin.identify(call!)
-      wait(for: [expectation], timeout: 1);
-    })();
-
-    
-//    ({
-//      let expectation = XCTestExpectation(description: "[IDENTIFY] NO_USER_ID");
-//      let call = CAPPluginCall(callbackId: "test", options: [
-//        "key": "test_segment_key"
-//        ], success: { (result, call) in
-//          XCTFail("🤦‍♂️ Success callback should not have been called")
-//          expectation.fulfill()
-//      }, error: { (err: CAPPluginCallError?) in
-//        XCTAssertEqual(err!.message, "segment already set up");
-//        XCTAssertEqual(err!.data!["code"]! as? String, "[SET_UP] DUPE_CALL");
-//        expectation.fulfill()
-//      });
-//      plugin.setUp(call!)
-//      wait(for: [expectation], timeout: 1);
-//    })();
-//
-//
-//    ({
-//      let expectation = XCTestExpectation(description: "[IDENTIFY] SUCCESS");
-//      let call = CAPPluginCall(callbackId: "test", options: [
-//        "key": "test_segment_key"
-//        ], success: { (result, call) in
-//          XCTFail("🤦‍♂️ Success callback should not have been called")
-//          expectation.fulfill()
-//      }, error: { (err: CAPPluginCallError?) in
-//        XCTAssertEqual(err!.message, "segment already set up");
-//        XCTAssertEqual(err!.data!["code"]! as? String, "[SET_UP] DUPE_CALL");
-//        expectation.fulfill()
-//      });
-//      plugin.setUp(call!)
-//      wait(for: [expectation], timeout: 1);
-//    })();
-
+      plugin.setUp(call!);
+    };
   }
   
 }
